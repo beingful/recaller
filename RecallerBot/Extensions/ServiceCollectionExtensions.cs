45db5dbd9dd478d124bac3f57661ba63b769a059
Hanna Kasai <anna.bananna.kasayka@gmail.com>
2023-09-03 19:24:45 +0300

    no message

--- RecallerBot/Extensions/ServiceCollectionExtensions.cs
+++ RecallerBot/Extensions/ServiceCollectionExtensions.cs
@@ -17,14 +17,13 @@ internal static class ServiceCollectionExtensions
     public static IServiceCollection AddAzureAuthentication(this IServiceCollection services, ConfigurationManager configuration)
     {
         services
-            .AddAuthorization()
             .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
             .AddMicrosoftIdentityWebApp(configuration)
             .EnableTokenAcquisitionToCallDownstreamApi()
             .AddMicrosoftGraph()
             .AddInMemoryTokenCaches();
         
-        return services;
+        return services.AddAuthorization();
     }
 
     public static IServiceCollection SetConfiguration(this IServiceCollection services) =>
